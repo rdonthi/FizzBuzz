bc4963f20a90a855336b341385c60a3ab8596672
ranjih.donthi <ranjih.donthi@accenture.com>
2018-04-09 15:26:00 +0100

    Changes

---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public string Generate(int num)
+
+        {
+            string result = num.ToString();
+            Console.Read();
+            {
+                if ((num % 3 == 0) && (num % 5 == 0))
+                    return "FizzBuzz";
+                if (num % 3 == 0)
+                    return "Fizz";
+                if (num % 5 == 0)
+                    return "Buzz";
+
+            }
+            return result;
+        }
+
+    }
+}
---
+++ FizzBuzzer.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzerTests
+    {
+        [TestCase]
+        public void Generate()
+        {
+            FizzBuzzer test = new FizzBuzzer();
+                  
+            Assert.AreEqual("Fizz", test.Generate(3));
+            Assert.AreEqual("Buzz", test.Generate(5));
+            Assert.AreEqual("FizzBuzz", test.Generate(15));
+            Assert.AreEqual("1", test.Generate(1));
+        }
+    }
+}
