56d8fef61322086465c35265898209c8580ce895
ranjih.donthi <ranjih.donthi@accenture.com>
2018-04-10 12:53:39 +0100

    Package Updates

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -9,21 +9,16 @@ namespace FizzBuzz
     public class FizzBuzzer
     {
         public string Generate(int num)
-
         {
             string result = num.ToString();
-            Console.Read();
+
             {
-                if ((num % 3 == 0) && (num % 5 == 0))
-                    return "FizzBuzz";
-                if (num % 3 == 0)
-                    return "Fizz";
-                if (num % 5 == 0)
-                    return "Buzz";
-
+                if ((num % 3 == 0) && (num % 5 == 0)) return "FizzBuzz";
+                if (num % 3 == 0) return "Fizz";
+                if (num % 5 == 0) return "Buzz";
+                else return result;
             }
-            return result;
         }
-
     }
 }
+
--- FizzBuzzer.Tests/FizzBuzzerTests.cs
+++ FizzBuzzer.Tests/FizzBuzzerTests.cs
@@ -1,21 +1,25 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzz;
 using NUnit.Framework;
 
 namespace FizzBuzz
 {
     [TestFixture]
-    public class FizzBuzzerTests
+    class FizzBuzzerTest
     {
         [TestCase]
         public void Generate()
         {
             FizzBuzzer test = new FizzBuzzer();
-                  
             Assert.AreEqual("Fizz", test.Generate(3));
             Assert.AreEqual("Buzz", test.Generate(5));
             Assert.AreEqual("FizzBuzz", test.Generate(15));
             Assert.AreEqual("1", test.Generate(1));
+            Assert.AreEqual("2", test.Generate(2));
         }
     }
 }
